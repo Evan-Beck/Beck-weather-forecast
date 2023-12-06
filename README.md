b8510c82d1107b2518368080c5548d9055befeac
unknown <ebeck703@gmail.com>
2023-12-06 16:26:31 -0500

    All buttons work and display 5 forecast boxes with temperature, humidity, wind, and weather icons. The search button and APIs are linked and work. README is finished, just have to add screenshots and live link.

---
+++ README.md
@@ -0,0 +1,24 @@
+USER STORY - AS A traveler
+
+I WANT to see the weather outlook for multiple cities
+SO THAT I can plan a trip accordingly
+
+The current product of this weather tracker allows the user to search for any city and upon clicking the search button, the user will be presented with a 5-day forecast of the entered city. 
+
+
+Acceptance Criteria -
+
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+
+The user is also presented with 8 clickable buttons of heavily populated or commonly searched for U.S. cities. When the user clicks one of these buttons, or types in a city and presses enter, the display will show - 
+
+5 Forecast boxes for the current day and upcoming days 4 days. This includes the date, an  icon for the kind of weather to expect, temperature, wind speed, and humidity percentage. 
+
--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// Function to convert temperature from Kelvin to Fahrenheit.
+function kelvinToFahrenheit(kelvin) {
+    return (kelvin - 273.15) * 9/5 + 32;
+}
+
 // Function to fetch and display the weather data.
 function fetchWeatherData(cityName) {
     const apiKey = '8cbc36a25efeb4b9e5921e0a757cfbf9';
@@ -8,6 +13,7 @@ function fetchWeatherData(cityName) {
         .then(data => {
             // Updates the forecast section with the retrieved data.
             updateForecast(data);
+            document.getElementById('cityForecastHeading').textContent = `5-Day Forecast for ${cityName}`;
         })
         .catch(error => {
             console.error('Error fetching forecast:', error);
@@ -17,7 +23,7 @@ function fetchWeatherData(cityName) {
 
 // Function to update the forecast section.
 function updateForecast(data) {
-    const forecastElement = document.getElementById('forecast');
+    const forecastElement = document.getElementById('forecastBoxes');
 
     // Clears previous forecast data.
     forecastElement.innerHTML = '';
@@ -26,23 +32,53 @@ function updateForecast(data) {
     for (let i = 0; i < data.list.length; i += 8) {
         const forecastItem = data.list[i];
         const date = new Date(forecastItem.dt * 1000); 
+        const iconCode = forecastItem.weather[0].icon;
+ // Convert temperature from Kelvin to Fahrenheit.
+        const temperatureFahrenheit = kelvinToFahrenheit(forecastItem.main.temp)
 
+        // Put the image source under the date so that icons could display along with the display boxes.
         const forecastHtml = `
         <div class="forecast-item">
         <p>Date: ${date.toDateString()}</p>
-        <p>Temperature: ${forecastItem.main.temp} K</p>
-        <!-- Add other forecast information as needed -->
+        <img src="https://openweathermap.org/img/wn/${iconCode}@2x.png" alt="Weather Icon">
+        <p>Temperature: ${temperatureFahrenheit.toFixed(2)} °F</p>
+        <p>Wind Speed: ${forecastItem.wind.speed} m/s</p>
+        <p>Humidity: ${forecastItem.main.humidity}%</p> 
  </div>
-`;
+`; 
+// No other elements needed for weather display in DIVs above.
         forecastElement.innerHTML += forecastHtml;
     }
 }
 
+// This function generates city buttons for populated states, along with a const to grab cityButtons element in HTML so it is appended.
+function generateCityButtons () {
+    const usCities = [
+        'New york', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix', 'Philadelphia', "San Antonio", 'San Diego'
+    ];
+    const cityButtonsElement = document.getElementById('cityButtons');
+// Loop through each city in the list. Also added buttons and a click event listener to fetch the weather data. 
+    usCities.forEach(city => {
+        const button = document.createElement('button');
+        button.classList.add('city-button');
+        button.textContent = city;
+        button.addEventListener('click', function() {
+            console.log(`Button clicked for city: ${city}`);
+            fetchWeatherData(city);
+    });
+    cityButtonsElement.appendChild(button);
+});
+console.log('City buttons generated.');
+}
+
+generateCityButtons();
 
 document.getElementById('cityForm').addEventListener('submit', function (event) {
     event.preventDefault();
 
     const cityName = document.getElementById('cityInput').value;
+
+    console.log(`Form submitted with city name: ${cityName}`);
     
     fetchWeatherData(cityName);
 });
