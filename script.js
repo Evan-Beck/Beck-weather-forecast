3451dba61bb6980cefa70322f010d0a85e35c491
unknown <ebeck703@gmail.com>
2023-12-05 19:34:06 -0500

    Added color coding to the search bar and got the weather forecast to display. Still need to do some work in CSS with the color coding.

--- script.js
+++ script.js
@@ -1,28 +1,48 @@
-// Function to fetch and display weather data
+// Function to fetch and display the weather data.
 function fetchWeatherData(cityName) {
     const apiKey = '8cbc36a25efeb4b9e5921e0a757cfbf9';
-
-
-    const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
-
     const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`;
-
-    fetch(currentWeatherUrl)
-        .then(response => response.json())
-        .then(data => {
-            updateCurrentWeather(data);
-        })
-        .catch(error => {
-            console.error('Error fetching current weather:', error);
-        });
 
     fetch(forecastUrl)
         .then(response => response.json())
         .then(data => {
+            // Updates the forecast section with the retrieved data.
             updateForecast(data);
         })
         .catch(error => {
             console.error('Error fetching forecast:', error);
+            // Catch error will display a message if true.
         });
 }
 
+// Function to update the forecast section.
+function updateForecast(data) {
+    const forecastElement = document.getElementById('forecast');
+
+    // Clears previous forecast data.
+    forecastElement.innerHTML = '';
+
+    // Loop through the 5-day forecast.
+    for (let i = 0; i < data.list.length; i += 8) {
+        const forecastItem = data.list[i];
+        const date = new Date(forecastItem.dt * 1000); 
+
+        const forecastHtml = `
+        <div class="forecast-item">
+        <p>Date: ${date.toDateString()}</p>
+        <p>Temperature: ${forecastItem.main.temp} K</p>
+        <!-- Add other forecast information as needed -->
+ </div>
+`;
+        forecastElement.innerHTML += forecastHtml;
+    }
+}
+
+
+document.getElementById('cityForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const cityName = document.getElementById('cityInput').value;
+    
+    fetchWeatherData(cityName);
+});
